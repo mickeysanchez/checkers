bdbb6271ebc6222e29d8e7015f9bd7f149b495db
App Academy Student <student@appacademy.io>
2014-01-09 09:13:17 -0500

    outlines main game loop

--- checkers.rb
+++ checkers.rb
@@ -5,10 +5,23 @@ class Checkers
   def initialize(player1, player2)
     @board = Board.new
     @color_of_current_player = :white
+    @players = [player1, player2]
   end
 
   def run
+    until game_over?
+      @board.display
+      @players.first.choose_move
+      @players.rotate!
+      switch(@color_of_current_player)
+    end
+  end
 
+  private
+
+  def switch(color)
+    @color_of_current_player =
+    (@color_of_current_player == :white) ? :black : :white
   end
 end
 
