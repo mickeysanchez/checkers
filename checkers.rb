69237b4134367ddc5535dadff32d909d8deda92e
App Academy Student <student@appacademy.io>
2014-01-09 09:24:04 -0500

    finishes outlining game loop

--- checkers.rb
+++ checkers.rb
@@ -15,6 +15,8 @@ class Checkers
       @players.rotate!
       switch(@color_of_current_player)
     end
+
+    puts "#{@players.first.name}, #{@color_of_current_player}, has been defeated."
   end
 
   private
@@ -23,14 +25,29 @@ class Checkers
     @color_of_current_player =
     (@color_of_current_player == :white) ? :black : :white
   end
+
+  def game_over?
+    false
+    # when there are no more pieces of color @color_of_current_player
+  end
 end
 
 class HumanPlayer
+  attr_reader :name
+
   def initialize(name)
     @name = name
   end
 
   def choose_move
-
+    "From:"
+    # chooses two coordinates.
+    # doesn't allow either coordinate to be invalid.
+    "To:"
   end
 end
+
+h1 = HumanPlayer.new("Mickey")
+h2 = HumanPlayer.new("Other Guy")
+c = Checkers.new(h1,h2)
+c.run
