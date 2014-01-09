b82dbdb998e7f7ab3e30a476e96621a1f3a4d35b
App Academy Student <student@appacademy.io>
2014-01-09 09:02:22 -0500

    adds human player class

--- checkers.rb
+++ checkers.rb
@@ -2,7 +2,22 @@ require_relative "board"
 require_relative "piece"
 
 class Checkers
-  def initialize
+  def initialize(player1, player2)
     @board = Board.new
+    @color_of_current_player = :white
+  end
+
+  def run
+
+  end
+end
+
+class HumanPlayer
+  def initialize(name)
+    @name = name
+  end
+
+  def choose_move
+
   end
 end
