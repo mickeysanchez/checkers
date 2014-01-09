52b8c4540847c365b4f8025810c372b2463081d1
App Academy Student <student@appacademy.io>
2014-01-09 09:36:00 -0500

    sets up board boundaries

--- board.rb
+++ board.rb
@@ -3,10 +3,23 @@ class Board
 
   def initialize
     @rows = Array.new(BOARD_SIZE) {Array.new(BOARD_SIZE)}
+    populate_rows
   end
 
   def [](pos)
+    raise "invalid pos" unless valid?(pos)
     i, j = pos
     @rows[i][j]
+  end
+
+  private
+
+  def populate_rows
+    # sets up initial game state
+  end
+
+  def valid?(pos)
+    y, x = pos
+    y < BOARD_SIZE && y >= 0 && x < BOARD_SIZE && x >= 0
   end
 end
