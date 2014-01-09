b0112bb97a612227a7dc43b513457b53ed55b956
App Academy Student <student@appacademy.io>
2014-01-09 13:10:09 -0500

    refactors pieces to hold their own current position. board reads from an array of pieces and places them based on their current position

--- board.rb
+++ board.rb
@@ -5,11 +5,13 @@ class Board
 
   def initialize
     @rows = Array.new(BOARD_SIZE) {Array.new(BOARD_SIZE) {nil}}
+    @pieces = []
+    create_pieces
     populate_rows
   end
 
   def [](pos)
-    raise "invalid pos" unless valid?(pos)
+    raise "pos not on board" unless on_board?(pos)
     i, j = pos
     @rows[i][j]
   end
@@ -33,29 +35,45 @@ class Board
     puts
   end
 
+  def populate_rows
+    de_populate_rows
+    @pieces.each do |piece|
+      y, x = piece.current_pos
+      @rows[y][x] = piece
+    end
+  end
+
   private
 
-  def populate_rows
-    # could refactor this using board size if i wanted to have
-    # some weird big chess game.
+  def create_pieces
+    0.upto(BOARD_SIZE-1) do |y|
+      next if y > 3 && y < BOARD_SIZE - 4
 
-    @rows.each_with_index do |row, y|
-      next if y > 3 && y < 6
-      row.each_index do |x|
+      0.upto(BOARD_SIZE-1) do |x|
         if x.odd? && y.even? && y < 4
-          @rows[y][x] = Piece.new(:black)
+          piece = Piece.new(:black, [y,x])
         elsif x.even? && y.odd? && y < 4
-          @rows[y][x] = Piece.new(:black)
+          piece = Piece.new(:black, [y,x])
         elsif x.odd? && y.even? && y > 5
-          @rows[y][x] = Piece.new
+          piece = Piece.new(:white, [y,x])
         elsif x.even? && y.odd? && y > 5
-          @rows[y][x] = Piece.new
+          piece = Piece.new(:white, [y,x])
         end
+
+        @pieces << piece unless piece == nil
       end
     end
   end
 
-  def valid?(pos)
+  def de_populate_rows
+    @rows.each_with_index do |row, y|
+      row.each_index do |x|
+        @rows[y][x] = nil
+      end
+    end
+  end
+
+  def on_board?(pos)
     y, x = pos
     y < BOARD_SIZE && y >= 0 && x < BOARD_SIZE && x >= 0
   end
--- piece.rb
+++ piece.rb
@@ -1,20 +1,22 @@
 class Piece
-  attr_reader :color, :icon
+  attr_reader :color, :icon, :current_pos
 
-  def initialize(color = :white)
+  def initialize(color, pos)
     @color = color
+    @current_pos = pos
     @king = false
     @icon = (@color == :white) ? "\u25CB" : "\u25CF"
   end
 
-  def slide(to, board)
-    boar
+  def slide(to)
   end
 
-  def jump(to, board)
+  def jump(to)
   end
 
   def move_diffs
+    # white always starts on bottom
+    # hence, it can only move up
     if @color == :white && @king == false
       [[-1, 1],[-1, 1]]
     elsif @color == :black && @king == false
